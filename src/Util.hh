36c965e05291d7a7b34a136df882a23921dcb806
Vladimir Marek <vladimir.marek@sun.com>
2008-05-18 19:54:43 +0200

    Add check for timersub as it is not available on Solaris 9
    I tried to compile pekwm on Solaris 9 and hat to tweak few
    bits and pieces... Second, Solaris does not have timersub function.

--- src/Util.hh
+++ src/Util.hh
@@ -82,4 +82,16 @@ namespace Util {
 int setenv(const char *name, const char *value, int overwrite);
 #endif // HAVE_SETENV
 
+#ifndef HAVE_TIMERSUB
+#define timersub(a, b, result)                                                \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                          \
+    if ((result)->tv_usec < 0) {                                              \
+      --(result)->tv_sec;                                                     \
+      (result)->tv_usec += 1000000;                                           \
+    }                                                                         \
+  } while (0)
+#endif // HAVE_TIMERSUB
+
 #endif // _UTIL_HH_
