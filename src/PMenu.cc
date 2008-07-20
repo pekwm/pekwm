433fb988d2003673423daa111e20ce5cf6239b0e
Claes Nästén <me@pekdon.net>
2008-07-20 18:04:41 +0200

    Start on fixed width menus to be used in searchdialog for displaying results.

--- src/PMenu.cc
+++ src/PMenu.cc
@@ -64,6 +64,7 @@ PMenu::PMenu(Display *dpy, Theme *theme, const std::wstring &title,
       _name(name),
       _menu_parent(0), _menu_wo(0),
       _menu_bg_fo(None), _menu_bg_un(None), _menu_bg_se(None),
+      _menu_width(0),
       _item_height(0), _item_width_max(0), _item_width_max_avail(0),
       _icon_width(0), _icon_height(0),
       _separator_height(0),
@@ -372,6 +373,11 @@ PMenu::buildMenuCalculate(void)
         }
     }
 
+    // FIXME: Remove extra padding from calculation
+    if (_menu_width) {
+      _item_width_max = _menu_width;
+    }
+
     // Make sure icon width and height are not larger than configured.
     if (Config::instance()->getMenuIconWidth()) {
       _icon_width = std::min(Config::instance()->getMenuIconWidth(), _icon_width);
@@ -403,8 +409,9 @@ PMenu::buildMenuCalculate(void)
 
     height = (_item_height * _size) + (_separator_height * sep);
 
-    // check if menu should have more than one row
-    if ((height + getTitleHeight()) > PScreen::instance()->getHeight()) {
+    // Check if menu should have more than one row, this does not apply on
+    // static width menus.
+    if (! _menu_width && (height + getTitleHeight()) > PScreen::instance()->getHeight()) {
         _cols = height / (PScreen::instance()->getHeight() - getTitleHeight());
         if ((height % (PScreen::instance()->getHeight() - getTitleHeight())) != 0) {
             ++_cols;
