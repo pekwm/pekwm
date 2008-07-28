d5ce1679032b06b6cf5195623ffc55c5d936dcde
Andreas <ioerror@lavabit.com>
2008-07-28 21:48:22 +0200

    Unifies the attribute names of CfgParserKey (underscore prefix).

--- src/CfgParserKey.cc
+++ src/CfgParserKey.cc
@@ -23,7 +23,7 @@ using std::numeric_limits;
 
 //! @brief Parses and store integer value.
 //! @param or_value Reference to string representing integer value.
-//! @return true on success, else false and m_ir_set to m_i_default.
+//! @return true on success, else false and _ir_set to _i_default.
 void
 CfgParserKeyInt::parse_value (const std::string &or_value)
 throw (std::string&)
@@ -37,77 +37,77 @@ throw (std::string&)
     // Check for validity, 0 is returned on failiure with endptr set to the
     // beginning of the string, else we are (semi) ok.
     if ((value == 0) && (endptr == or_value.c_str ())) {
-        m_ir_set = m_i_default;
+        _ir_set = _i_default;
 
     } else {
-        if (value < m_i_value_min) {
-            m_ir_set = m_i_value_min;
+        if (value < _i_value_min) {
+            _ir_set = _i_value_min;
             throw string ("value to low, min value "
-                          + Util::to_string<int> (m_i_value_min));
-        } if (value > m_i_value_max)  {
-            m_ir_set = m_i_value_max;
+                          + Util::to_string<int> (_i_value_min));
+        } if (value > _i_value_max)  {
+            _ir_set = _i_value_max;
             throw string ("value to high, max value "
-                          + Util::to_string<int> (m_i_value_max));
+                          + Util::to_string<int> (_i_value_max));
         }
     }
 
-    m_ir_set = value;
+    _ir_set = value;
 }
 
-//! @brief Parses or_value and sets m_br_set.
+//! @brief Parses or_value and sets _br_set.
 //! Boolean true is represented either by case insensitive true or 1.
 //! Boolean false is represented either by case insensitive false or 0.
 //! @param or_value Value to parse.
-//! @return true on success, else false and m_br_set set to m_b_default.
+//! @return true on success, else false and _br_set set to _b_default.
 void
 CfgParserKeyBool::parse_value (const std::string &or_value)
 throw (std::string&)
 {
     if ((or_value == "1") || !strcasecmp (or_value.c_str(), "TRUE")) {
-        m_br_set = true;
+        _br_set = true;
     } else if ((or_value == "0") || !strcasecmp (or_value.c_str(), "FALSE")) {
-        m_br_set = false;
+        _br_set = false;
     } else  {
-        m_br_set = m_b_default;
+        _br_set = _b_default;
         throw string ("not bool value");
     }
 }
 
-//! @brief Parses or_value and sets m_or_set.
+//! @brief Parses or_value and sets _or_set.
 //! @param or_value Value to parse.
-//! @return true on success, else false and m_or_set set to m_o_default.
+//! @return true on success, else false and _or_set set to _o_default.
 void
 CfgParserKeyString::parse_value (const std::string &or_value)
 throw (std::string&)
 {
-    m_or_set = or_value;
+    _or_set = or_value;
 
-    if ((m_i_length_min != numeric_limits<int>::min ())
-            && (static_cast<int> (m_or_set.size()) < m_i_length_min)) {
-        m_or_set = m_o_default;
+    if ((_i_length_min != numeric_limits<int>::min ())
+            && (static_cast<int> (_or_set.size()) < _i_length_min)) {
+        _or_set = _o_default;
         throw string ("string too short, min length "
-                      + Util::to_string<int> (m_i_length_min));
+                      + Util::to_string<int> (_i_length_min));
     }
-    if ((m_i_length_max != numeric_limits<int>::max ())
-            && (static_cast<int> (m_or_set.size()) > m_i_length_max))  {
-        m_or_set = m_o_default;
+    if ((_i_length_max != numeric_limits<int>::max ())
+            && (static_cast<int> (_or_set.size()) > _i_length_max))  {
+        _or_set = _o_default;
         throw string ("string too long, max length "
-                      + Util::to_string<int> (m_i_length_max));
+                      + Util::to_string<int> (_i_length_max));
     }
 }
 
-//! @brief Parses or_value and sets m_or_set.
+//! @brief Parses or_value and sets _or_set.
 //! @param or_value Value to parse.
-//! @return true on success, else false and m_or_set set to m_o_default.
+//! @return true on success, else false and _or_set set to _o_default.
 void
 CfgParserKeyPath::parse_value (const std::string &or_value)
 throw (std::string&)
 {
     if (or_value.size ()) {
-        m_or_set = or_value;
-        Util::expandFileName (m_or_set);
+        _or_set = or_value;
+        Util::expandFileName (_or_set);
     } else {
-        m_or_set = m_o_default;
+        _or_set = _o_default;
         throw string ("path too short");
     }
 }
--- src/CfgParserKey.hh
+++ src/CfgParserKey.hh
@@ -29,21 +29,21 @@ enum CfgParserKeyType {
 class CfgParserKey {
 public:
     //! @brief CfgParserKey constructor.
-    CfgParserKey (const char *op_name) : m_op_name (op_name) { }
+    CfgParserKey (const char *op_name) : _op_name (op_name) { }
     //! @brief CfgParserKey destructor.
     virtual ~CfgParserKey (void) { }
 
     //! @brief Returns Key name.
-    const char *get_name (void) const { return m_op_name; }
+    const char *get_name (void) const { return _op_name; }
     //! @brief Returns Key type.
-    const CfgParserKeyType get_type (void) const { return m_type; }
+    const CfgParserKeyType get_type (void) const { return _type; }
 
     //! @brief Parses or_value and sets Key value.
     virtual void parse_value (const std::string &or_value) throw (std::string&) { }
 
 protected:
-    CfgParserKeyType m_type; //!< Key type.
-    const char *m_op_name; //!< Key name.
+    CfgParserKeyType _type; //!< Key type.
+    const char *_op_name; //!< Key name.
 };
 
 
@@ -56,9 +56,10 @@ public:
                      const int i_value_min = std::numeric_limits<int>::min (),
                      const int i_value_max = std::numeric_limits<int>::max ()) :
             CfgParserKey (op_name),
-            m_ir_set (ir_set), m_i_default (i_default),
-    m_i_value_min (i_value_min), m_i_value_max (i_value_max) {
-        m_type = KEY_INT;
+            _ir_set(ir_set), _i_default(i_default),
+            _i_value_min(i_value_min), _i_value_max(i_value_max)
+    {
+        _type = KEY_INT;
     }
     //! @brief CfgParserKeyInt destructor.
     virtual ~CfgParserKeyInt (void) { }
@@ -66,10 +67,10 @@ public:
     virtual void parse_value (const std::string &or_value) throw (std::string&);
 
 private:
-    int &m_ir_set; //! Reference to store parsed value in.
-    const int m_i_default; //! Default value.
-    const int m_i_value_min; //! Minimum value.
-    const int m_i_value_max; //! Maximum value.
+    int &_ir_set; //! Reference to store parsed value in.
+    const int _i_default; //! Default value.
+    const int _i_value_min; //! Minimum value.
+    const int _i_value_max; //! Maximum value.
 };
 
 //! @brief CfgParser Key boolean value parser.
@@ -78,16 +79,18 @@ public:
     //! @brief CfgParserKeyBool constructor.
     CfgParserKeyBool (const char *op_name,
                       bool &ib_set, const bool b_default = false) :
-            CfgParserKey (op_name),
-    m_br_set (ib_set), m_b_default (b_default) { m_type = KEY_BOOL; }
+            CfgParserKey(op_name), _br_set(ib_set), _b_default(b_default)
+    {
+        _type = KEY_BOOL;
+    }
     //! @brief CfgParserKeyBool destructor.
     virtual ~CfgParserKeyBool (void) { }
 
     virtual void parse_value (const std::string &or_value) throw (std::string&);
 
 private:
-    bool &m_br_set; //! Reference to stored parsed value in.
-    const bool m_b_default; //! Default value.
+    bool &_br_set; //! Reference to stored parsed value in.
+    const bool _b_default; //! Default value.
 };
 
 //! @brief CfgParser Key string value parser.
@@ -98,10 +101,10 @@ public:
                         std::string &or_set, const std::string o_default = "",
                         const int i_length_min = std::numeric_limits<int>::min (),
                         const int i_length_max = std::numeric_limits<int>::max ()) :
-            CfgParserKey (op_name),
-            m_or_set (or_set), m_o_default (o_default),
-    m_i_length_min (i_length_min), m_i_length_max (i_length_max) {
-        m_type = KEY_STRING;
+            CfgParserKey(op_name), _or_set(or_set), _o_default(o_default),
+            _i_length_min(i_length_min), _i_length_max(i_length_max)
+    {
+        _type = KEY_STRING;
     }
     //! @brief CfgParserKeyString destructor.
     virtual ~CfgParserKeyString (void) { }
@@ -109,10 +112,10 @@ public:
     virtual void parse_value (const std::string &or_value) throw (std::string&);
 
 private:
-    std::string &m_or_set; //!< Reference to store parsed value in.
-    const std::string m_o_default; //!< Default value.
-    const int m_i_length_min; //!< Minimum lenght of string.
-    const int m_i_length_max; //!< Maximum length of string.
+    std::string &_or_set; //!< Reference to store parsed value in.
+    const std::string _o_default; //!< Default value.
+    const int _i_length_min; //!< Minimum lenght of string.
+    const int _i_length_max; //!< Maximum length of string.
 };
 
 //! @brief CfgParser Key path parser.
@@ -121,16 +124,18 @@ public:
     //! @brief CfgParserKeyPath constructor.
     CfgParserKeyPath (const char *op_name,
                       std::string &or_set, const std::string o_default = "") :
-            CfgParserKey (op_name),
-    m_or_set (or_set), m_o_default (o_default) { m_type = KEY_PATH; }
+            CfgParserKey(op_name), _or_set(or_set), _o_default(o_default)
+    {
+        _type = KEY_PATH;
+    }
     //! @brief CfgParserKeyPath destructor.
     virtual ~CfgParserKeyPath (void) { }
 
     virtual void parse_value (const std::string &or_value) throw (std::string&);
 
 private:
-    std::string &m_or_set; //!< Reference to store parsed value in.
-    const std::string m_o_default; //!< Default value.
+    std::string &_or_set; //!< Reference to store parsed value in.
+    const std::string _o_default; //!< Default value.
 };
 
 #endif // _CFG_PARSER_KEY_HH_
