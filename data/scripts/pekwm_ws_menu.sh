daeac773e8767272d5d96dea27edd31dd7d0ae44
Claes Nästén <me@pekdon.net>
2008-08-05 18:44:43 +0200

    Remove perl scripts, start replacing with pure shell scripts.
    Removed both perl scripts used in menus and implemented pekwm_ws_menus
    as a .sh script. Old pekwm_themeset.sh script needs to be fixed so that
    it does not depend on bash.

---
+++ data/scripts/pekwm_ws_menu.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Copyright © 2008 the pekwm development team
+#
+
+if test "${1}" = "send"; then
+  action="SendToWorkspace"
+elif test "${1}" = "goto"; then
+  action="GotoWorkspace"
+else
+  echo "usage: $0 goto|send dynamic"
+  exit 1
+fi
+
+if test "${2}" = "dynamic"; then
+  echo "Dynamic {"
+fi
+
+num_workspaces="$(xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{ print $3 }')"
+
+i=1;
+while test "${i}" -le "${num_workspaces}"; do
+  echo "Entry = \"Workspace $i\" { Actions = \"${action} ${i}\" }"
+
+  i=$(($i + 1))
+done
+
+
+if test "${2}" = "dynamic"; then
+  echo "}"
+fi
