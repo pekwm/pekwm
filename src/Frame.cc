pekwm/pekwm

commit 7bd7afc250a9f7578de7e38b5ed5925af8860de1

First try to get fullscreen detection, code has not yet been tested.
This introcuses a new option, FullscreenDetect to be put in the Screen
section. When True pekwm will try to detect non EWMH fullscreen requests
and make the window go to real fullscreen mode (removing decoration and
raising it).

src/Frame.cc
@@ -1921,23 +1921,9 @@ Frame::handleConfigureRequest(XConfigureRequestEvent *ev, Client *client)
         return; // only handle the active client's events
     }
 
-    // size before position, as we rely on size when gravitating
-    if (! client->isCfgDeny(CFG_DENY_SIZE)) {
-        if ((ev->value_mask&CWWidth) || (ev->value_mask&CWHeight)) {
-            resizeChild(ev->width, ev->height);
-#ifdef HAVE_SHAPE
-            _client->setShaped(setShape());
-#endif // HAVE_SHAPE
-        }
-    }
-
-    if (! client->isCfgDeny(CFG_DENY_POSITION)) {
-        if ((ev->value_mask&CWX) || (ev->value_mask&CWY)) {
-            calcGravityPosition(_client->getXSizeHints()->win_gravity,
-                                ev->x, ev->y, _gm.x, _gm.y);
-            move(_gm.x, _gm.y);
-        }
-    }
+    // Handled geometry, this is handled seperatley due to fullscreen
+    // detection
+    handleConfigureRequestGeometry(ev, client);
 
     // update the stacking
     if (! client->isCfgDeny(CFG_DENY_STACKING)) {
@@ -1972,6 +1958,48 @@ Frame::handleConfigureRequest(XConfigureRequestEvent *ev, Client *client)
             }
         }
     }
+}
+
+/**
+ * Handle size and position part of configure request, detects
+ * fullscreen mode if detection is enabled.
+ */
+void
+Frame::handleConfigureRequestGeometry(XConfigureRequestEvent *ev, Client *client)
+{
+  // Look for fullscreen requests
+  long all_geometry = CWX|CWY|CWWidth|CWHeight;
+  bool is_fullscreen = false;
+  if (Config::instance()->isFullscreenDetect()
+      && !client->isCfgDeny(CFG_DENY_SIZE) && !client->isCfgDeny(CFG_DENY_POSITION)
+      && (ev->value_mask&all_geometry == all_geometry)) {
+    Geometry gm_request(ev->x, ev->y, ev->width, ev->height);
+    
+    if (gm_request == _scr->getScreenGeometry()
+	|| gm_request == _scr->getHeadGeometry(_scr->getNearestHead(ev->x, ev->y))) {
+      is_fullscreen = true;
+      setStateFullscreen(STATE_SET);
+    }
+  }
+
+  if (! is_fullscreen) {
+    // Remove fullscreen state if client changes it size
+    if (Config::instance()->isFullscreenDetect()) {
+      setStateFullscreen(STATE_UNSET);
+    }
+
+    if (!client->isCfgDeny(CFG_DENY_SIZE)
+	&& ((ev->value_mask&CWWidth) || (ev->value_mask&CWHeight))) {
+      resizeChild(ev->width, ev->height);
+      _client->setShaped(setShape());
+    }
+    if (!client->isCfgDeny(CFG_DENY_POSITION)
+	&& ((ev->value_mask&CWX) || (ev->value_mask&CWY))) {
+      calcGravityPosition(_client->getXSizeHints()->win_gravity,
+			  ev->x, ev->y, _gm.x, _gm.y);
+      move(_gm.x, _gm.y);
+    }
+  }
 }
 
 //! @brief

data/config.in
@@ -29,6 +29,7 @@ Screen {
 
     TrimTitle = "..."
     FullscreenAbove = "True"
+    FullscreenDetect = "True"
     EdgeSize = "1"
     EdgeIndent = "False"
     PixmapCacheSize = "20"

doc/config/configfile/keywords.xml
@@ -150,6 +150,12 @@
         </listitem>
       </varlistentry>
       <varlistentry>
+        <term>FullscreenDetect (boolean)</term>
+        <listitem>
+          <para>Toggles detection of broken fullscreen requests setting clients to fullscreen mode when requesting to be the size of the screen. Default true.</para>
+        </listitem>
+      </varlistentry>
+      <varlistentry>
         <term>EdgeSize (int) (int) (int) (int)</term>
         <listitem>
           <para>

src/Config.cc
@@ -54,6 +54,7 @@ Config::Config(void) :
         _screen_workspaces_per_row(0), _screen_workspace_name_default(L"Workspace"),
         _screen_edge_indent(false),
         _screen_doubleclicktime(250), _screen_fullscreen_above(true),
+        _screen_fullscreen_detect(true),
         _screen_showframelist(true),
         _screen_show_status_window(true), _screen_show_client_id(false),
         _screen_show_workspace_indicator(500), _screen_workspace_indicator_scale(16),
@@ -523,6 +524,7 @@ Config::loadScreen(CfgParser::Entry *section)
     o_key_list.push_back (new CfgParserKeyString ("TRIMTITLE",
                           _screen_trim_title));
     o_key_list.push_back (new CfgParserKeyBool ("FULLSCREENABOVE", _screen_fullscreen_above, true));
+    o_key_list.push_back (new CfgParserKeyBool ("FULLSCREENDETECT", _screen_fullscreen_detect, true));
     o_key_list.push_back (new CfgParserKeyBool ("SHOWFRAMELIST",
                           _screen_showframelist));
     o_key_list.push_back (new CfgParserKeyBool ("SHOWSTATUSWINDOW",

src/Config.hh
@@ -69,6 +69,7 @@ public:
     inline const std::string &getTrimTitle(void) const { return _screen_trim_title; }
 
     inline bool isFullscreenAbove(void) const { return _screen_fullscreen_above; }
+    inline bool isFullscreenDetect(void) const { return _screen_fullscreen_detect; }
 
     inline bool getShowFrameList(void) const { return _screen_showframelist; }
     inline bool isShowStatusWindow(void) const { return _screen_show_status_window; }
@@ -196,6 +197,7 @@ private:
     int _screen_doubleclicktime;
     std::string _screen_trim_title;
     bool _screen_fullscreen_above; //!< Flag to make fullscreen go above all windows. */
+    bool _screen_fullscreen_detect; /**< Flag to make configure request fullscreen detection. */
     bool _screen_showframelist;
     bool _screen_show_status_window;
     bool _screen_show_client_id; //!< Flag to display client ID in title.

src/Frame.hh
@@ -141,6 +141,7 @@ protected:
     // END - PDecor interface
 
 private:
+    void handleConfigureRequestGeometry(XConfigureRequestEvent *ev, Client *client);
     void recalcResizeDrag(int nx, int ny, bool left, bool top);
     void getMaxBounds(int &max_x,int &max_r, int &max_y, int &max_b);
     void calcSizeInCells(uint &width, uint &height);

src/PScreen.cc
@@ -86,7 +86,7 @@ PScreen::PVisual::getShiftPrecFromMask(ulong mask, int &shift, int &prec)
 //! @brief PScreen constructor
 PScreen::PScreen(Display *dpy) :
         _dpy(dpy), _fd(-1),
-        _screen(-1), _depth(-1), _width(0), _height(0),
+        _screen(-1), _depth(-1),
         _root(None), _visual(0), _colormap(None),
         _modifier_map(0),
         _num_lock(0), _scroll_lock(0),
@@ -111,8 +111,8 @@ PScreen::PScreen(Display *dpy) :
     _colormap = DefaultColormap(_dpy, _screen);
     _modifier_map = XGetModifierMapping(_dpy);
 
-    _width = WidthOfScreen(ScreenOfDisplay(_dpy, _screen));
-    _height = HeightOfScreen(ScreenOfDisplay(_dpy, _screen));
+    _screen_gm.width = WidthOfScreen(ScreenOfDisplay(_dpy, _screen));
+    _screen_gm.height = HeightOfScreen(ScreenOfDisplay(_dpy, _screen));
 
 #ifdef HAVE_SHAPE
     {
@@ -275,8 +275,8 @@ PScreen::updateGeometry(uint width, uint height)
   // and strut information is updated.
   initHeads();
 
-  _width = width;
-  _height = height;
+  _screen_gm.width = width;
+  _screen_gm.height = height;
   PWinObj::getRootPWinObj()->resize(width, height);
 
   updateStrut();
@@ -398,6 +398,17 @@ PScreen::getHeadInfo(uint head, Geometry &head_info)
     }
 }
 
+/**
+ * Same as getHeadInfo but returns Geometry instead of filling it in.
+ */
+Geometry
+PScreen::getHeadGeometry(uint head)
+{
+  Geometry gm(_screen_gm);
+  getHeadInfo(head, gm);
+  return gm;
+}
+
 //! @brief Fill information about head and the strut.
 void
 PScreen::getHeadInfoWithEdge(uint num, Geometry &head)
@@ -414,14 +425,14 @@ PScreen::getHeadInfoWithEdge(uint num, Geometry &head)
     head.x += strut_val;
     head.width -= strut_val;  
 
-    strut_val = ((head.x + head.width) == _width) ? std::max(_strut.right, strut.right) : strut.right;
+    strut_val = ((head.x + head.width) == _screen_gm.width) ? std::max(_strut.right, strut.right) : strut.right;
     head.width -= strut_val;
 
     strut_val = (head.y == 0) ? std::max(_strut.top, strut.top) : strut.top;
     head.y += strut_val;
     head.height -= strut_val;
 
-    strut_val = (head.y + head.height == _height) ? std::max(_strut.bottom, strut.bottom) : strut.bottom;
+    strut_val = (head.y + head.height == _screen_gm.height) ? std::max(_strut.bottom, strut.bottom) : strut.bottom;
     head.height -= strut_val;
 }
 
@@ -530,7 +541,7 @@ PScreen::initHeads(void)
         initHeadsXinerama();
 
         if (! _heads.size()) {
-            _heads.push_back(Head(0, 0, _width, _height));
+            _heads.push_back(Head(0, 0, _screen_gm.width, _screen_gm.height));
         }
     }
 }

src/PScreen.hh
@@ -107,8 +107,10 @@ public:
     inline Display* getDpy(void) const { return _dpy; }
     inline int getScreenNum(void) const { return _screen; }
     inline Window getRoot(void) const { return _root; }
-    inline uint getWidth(void) const { return _width; }
-    inline uint getHeight(void) const { return _height; }
+
+    const Geometry &getScreenGeometry(void) const { return _screen_gm; }
+    inline uint getWidth(void) const { return _screen_gm.width; }
+    inline uint getHeight(void) const { return _screen_gm.height; }
 
     inline int getDepth(void) const { return _depth; }
     inline PScreen::PVisual *getVisual(void) { return _visual; }
@@ -145,6 +147,7 @@ public:
     uint getNearestHead(int x, int y);
     uint getCurrHead(void);
     bool getHeadInfo(uint head, Geometry &head_info);
+    Geometry getHeadGeometry(uint head);
     void getHeadInfoWithEdge(uint head, Geometry &head_info);
     inline int getNumHeads(void) const { return _heads.size(); }
 
@@ -208,7 +211,8 @@ private:
     int _fd;
 
     int _screen, _depth;
-    uint _width, _height;
+
+  Geometry _screen_gm; /**< Screen geometry, no head information. */
 
     Window _root;
     PScreen::PVisual *_visual;

commit 375a575d8f1c77692cf6931ceeeb2825a957db1e

Close #84, unable to make mplayer window fullscreen.
I installed pekwm-0.1.8 and when I started watching movie with mplayer
and pressed 'f' (make mplayer fullscreen) the size of the window remains the
same, not fullscreen, although its content changed so that I could see the
top-left region of the movie in the small window. It looked like mplayer
incorrectly "thought" the size of the window changed.

The bug seems to be in the code introduced by the revision
7bd7afc250a9f7578de7e38b5ed5925af8860de1.

When debugging I discovered that sometimes ev->value_mask includes
CWStackMode and doesn't include CWY/X/Height/Width. In such case if the
window had been already maximized earlier then the code sets fullscreen
state to STATE_UNSET which is wrong.

The obvious fix (the attached one) is to set fullscreen state only if
one of CWY/X/Height/Width was found in ev->value_mask.

But I guess the actual error is in another place where somebody passes
ev->value_mask with anything unexpected (CWStackMode) althought it
should not.

What do you think?

P.S. The bug appears only when FullscreenDetect is True or unset (since
it defaults to True)

src/Frame.cc
@@ -1990,7 +1990,7 @@ void
 Frame::handleConfigureRequestGeometry(XConfigureRequestEvent *ev, Client *client)
 {
     // Look for fullscreen requests
-    long all_geometry = CWX|CWY|CWWidth|CWHeight;
+    const long all_geometry = CWX|CWY|CWWidth|CWHeight;
     bool is_fullscreen = false;
     if (Config::instance()->isFullscreenDetect()
         && ! client->isCfgDeny(CFG_DENY_SIZE)
@@ -2007,16 +2007,15 @@ Frame::handleConfigureRequestGeometry(XConfigureRequestEvent *ev, Client *client
     }
 
     if (! is_fullscreen) {
-        // Remove fullscreen state if client changes it size
-        if (Config::instance()->isFullscreenDetect()) {
-            setStateFullscreen(STATE_UNSET);
-        }
+        bool change_geometry = false;
 
         if (! client->isCfgDeny(CFG_DENY_SIZE)
                 && (ev->value_mask & (CWWidth|CWHeight)) ) {
 
             resizeChild(ev->width, ev->height);
             _client->setShaped(setShape());
+
+           change_geometry = true;
         }
 
         if (! client->isCfgDeny(CFG_DENY_POSITION)
@@ -2025,6 +2024,13 @@ Frame::handleConfigureRequestGeometry(XConfigureRequestEvent *ev, Client *client
             calcGravityPosition(_client->getXSizeHints()->win_gravity,
                                 ev->x, ev->y, _gm.x, _gm.y);
             move(_gm.x, _gm.y);
+
+           change_geometry = true;
+        }
+
+        // Remove fullscreen state if client changes it size
+        if (change_geometry && Config::instance()->isFullscreenDetect()) {
+            setStateFullscreen(STATE_UNSET);
         }
     }
 }