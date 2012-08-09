6e14f6bcf057ff75ba585f95549843a98b281296
Andreas Schlick <ioerror@lavabit.com>
2012-08-09 20:45:51 +0200

    Change std::list to std::vector in Observable.

--- src/Observable.hh
+++ src/Observable.hh
@@ -9,11 +9,7 @@
 #ifndef _OBSERVABLE_HH_
 #define _OBSERVABLE_HH_
 
-#ifdef HAVE_SLIST
-#include <slist>
-#else // HAVE_EXT_SLIST
-#include <ext/slist>
-#endif // HAVE_SLIST
+#include <vector>
 
 class Observer;
 
@@ -36,7 +32,7 @@ public:
     void removeObserver(Observer *observer); 
 
 private:
-    SLIST_NAMESPACE::slist<Observer*> _observers; /**< List of observers. */
+    std::vector<Observer*> _observers; /**< List of observers. */
 };
 
 #endif // _OBSERVABLE_HH_
