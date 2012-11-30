63c1ec9d988919d7292620cb3c12d8811de09bf2
Andreas Schlick <ioerror@lavabit.com>
2012-11-30 11:00:59 +0100

    Fix compilation with older gcc.
    The patch was provided by wallex. Close #318

--- src/x11.cc
+++ src/x11.cc
@@ -993,6 +993,8 @@ X11::getKeycodeFromMask(uint mask)
 /**
  * Wrapper for XKeycodeToKeysym and XkbKeycodeToKeysym depending on which one is available.
  */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 KeySym
 X11::getKeysymFromKeycode(KeyCode keycode)
 {
@@ -1001,12 +1003,9 @@ X11::getKeysymFromKeycode(KeyCode keycode)
         return XkbKeycodeToKeysym(_dpy, keycode, 0, 0);
     else
 #endif
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
         return XKeycodeToKeysym(_dpy, keycode, 0);
-#pragma GCC diagnostic pop
 }
+#pragma GCC diagnostic pop
 
 Display *X11::_dpy;
 bool X11::_honour_randr = false;
