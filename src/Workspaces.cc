ca6b22f5e3dba920fd805368ddfc0f652ced5a43
Claes Nästén <pekdon@gmail.com>
2021-11-29 20:35:38 +0100

    Keep _NET_CLIENT_LIST on last window. Close #110
    Unsetting _NET_CLIENT_LIST and _NET_CLIENT_LIST_STACKING when they end up
    empty does not work as expected on older versions of Tint2 (maybe other
    applications as well) so restore previous behavior setting empty properties.

--- src/Workspaces.cc
+++ src/Workspaces.cc
@@ -779,13 +779,11 @@ Workspaces::updateClientList(void)
 {
 	uint num;
 	Window *windows = buildClientList(num);
-	if (num == 0) {
-		X11::unsetProperty(X11::getRoot(), NET_CLIENT_LIST);
-		X11::unsetProperty(X11::getRoot(), NET_CLIENT_LIST_STACKING);
-	} else {
-		X11::setWindows(X11::getRoot(), NET_CLIENT_LIST, windows, num);
-		X11::setWindows(X11::getRoot(), NET_CLIENT_LIST_STACKING, windows, num);
-	}
+	// previously, the lists where unset when they ended up empty
+	// however some applications does not support this, one
+	// example being tint2 on Debian Stretch
+	X11::setWindows(X11::getRoot(), NET_CLIENT_LIST, windows, num);
+	X11::setWindows(X11::getRoot(), NET_CLIENT_LIST_STACKING, windows, num);
 	delete [] windows;
 }
 
@@ -797,11 +795,7 @@ Workspaces::updateClientStackingList(void)
 {
 	uint num;
 	Window *windows = buildClientList(num);
-	if (num == 0) {
-		X11::unsetProperty(X11::getRoot(), NET_CLIENT_LIST_STACKING);
-	} else {
-		X11::setWindows(X11::getRoot(), NET_CLIENT_LIST_STACKING, windows, num);
-	}
+	X11::setWindows(X11::getRoot(), NET_CLIENT_LIST_STACKING, windows, num);
 	delete [] windows;
 }
 
