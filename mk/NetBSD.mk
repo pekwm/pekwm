997d3ad332927c58a3f60e73d1aafcc5d85cce66
Claes Nästén <pekdon@gmail.com>
2022-03-05 20:36:31 +0100

    configure: Add freetype directory required on NetBSD 9

--- mk/NetBSD.mk
+++ mk/NetBSD.mk
@@ -1,7 +1,7 @@
 .POSIX:
 
 # compiler options
-X11_CFLAGS = -I/usr/X11R7/include
+X11_CFLAGS = -I/usr/X11R7/include -I/usr/X11R7/include/freetype2
 X11_LDFLAGS = -L/usr/X11R7/lib -lX11 -lXpm -lXft
 PKG_CFLAGS = -I/usr/pkg/include
 PKG_LDFLAGS = -L/usr/pkg/lib -lpng16 -ljpeg
