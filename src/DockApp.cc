pekwm/pekwm

commit ac6a6378867c84148bf5f5d77eabb3bfcdea7a81

Create and use X11::createWmWindow
Instead of using X11::createSimpleWindow which does not set
override_redirect to true and also to avoid having to specify a long
list of arguments on X11::createWindow add X11::createWmWindow to be
used for window manager created windows.

src/DockApp.cc
@@ -82,16 +82,10 @@ DockApp::DockApp(Window win) :
 	updateSize();
 
 	// create parent window which is going to hold the border
-	XSetWindowAttributes sattr;
-	sattr.override_redirect = True;
-	sattr.event_mask =
-		SubstructureRedirectMask|ButtonPressMask|ButtonMotionMask;
-
-	_window =
-		X11::createWindow(X11::getRoot(),
-				  _gm.x, _gm.y, _gm.width, _gm.height, 0,
-				  CopyFromParent, InputOutput, CopyFromParent,
-				  CWOverrideRedirect|CWEventMask, &sattr);
+	_window = X11::createWmWindow(X11::getRoot(),
+				      _gm.x, _gm.y, _gm.width, _gm.height,
+				      SubstructureRedirectMask|ButtonPressMask|
+				      ButtonMotionMask);
 
 	// initial makeup
 	repaint();

src/InputDialog.cc
@@ -131,15 +131,13 @@ InputDialog::InputDialog(const std::string &title)
 	setTitle(title);
 
 	_text_wo = new PWinObj(true);
-	XSetWindowAttributes attr;
-	attr.override_redirect = false;
-	attr.event_mask = ButtonPressMask|ButtonReleaseMask|ButtonMotionMask|
-		FocusChangeMask|KeyPressMask|KeyReleaseMask;
-	_text_wo->setWindow(X11::createWindow(_window, 0, 0, 1, 1, 0,
-					      CopyFromParent, InputOutput,
-					      CopyFromParent,
-					      CWOverrideRedirect|CWEventMask,
-					      &attr));
+
+	Window window =
+		X11::createWmWindow(_window, 0, 0, 1, 1,
+				    ButtonPressMask|ButtonReleaseMask|
+				    ButtonMotionMask|FocusChangeMask|
+				    KeyPressMask|KeyReleaseMask);
+	_text_wo->setWindow(window);
 
 	addChild(_text_wo);
 	addChildWindow(_text_wo->getWindow());

src/ManagerWindows.cc
@@ -1,6 +1,6 @@
 //
 // ManagerWindows.cc for pekwm
-// Copyright (C) 2009-2020 Claes Nästén <pekdon@gmail.com>
+// Copyright (C) 2009-2022 Claes Nästén <pekdon@gmail.com>
 //
 // This program is licensed under the GNU GPL.
 // See the LICENSE file for more information.
@@ -41,18 +41,9 @@ HintWO::HintWO(Window root)
 {
 	_type = WO_SCREEN_HINT;
 	setLayer(LAYER_NONE);
-	_sticky = true; // Hack, do not map/unmap this window
-	_iconified = true; // Hack, to be ignored when placing
 
-	// Create window
-	_window = X11::createSimpleWindow(root,
-					  -200, -200, 5, 5, 0, 0, 0);
-
-	// Remove override redirect from window
-	XSetWindowAttributes attr;
-	attr.override_redirect = True;
-	attr.event_mask = PropertyChangeMask;
-	X11::changeWindowAttributes(_window, CWEventMask|CWOverrideRedirect, attr);
+	_window = X11::createWmWindow(X11::getRoot(), -200, -200, 5, 5,
+				      PropertyChangeMask);
 
 	// Set hints not being updated
 	X11::setUtf8String(_window, NET_WM_NAME, WM_NAME);
@@ -594,15 +585,9 @@ EdgeWO::EdgeWO(RootWO* root_wo, EdgeType edge, bool set_strut,
 	_iconified = true; // hack, to be ignored when placing
 	_focusable = false; // focusing input only windows crashes X
 
-	XSetWindowAttributes sattr;
-	sattr.override_redirect = True;
-	sattr.event_mask =
-		EnterWindowMask|LeaveWindowMask|ButtonPressMask|ButtonReleaseMask;
-
-	_window = X11::createWindow(root_wo->getWindow(),
-				    0, 0, 1, 1, 0,
-				    CopyFromParent, InputOnly, CopyFromParent,
-				    CWOverrideRedirect|CWEventMask, &sattr);
+	_window = X11::createWmWindow(root_wo->getWindow(), 0, 0, 1, 1,
+				      EnterWindowMask|LeaveWindowMask|
+				      ButtonPressMask|ButtonReleaseMask);
 
 	configureStrut(set_strut);
 	_root_wo->addStrut(&_strut);

src/PDecor.cc
@@ -49,14 +49,10 @@ PDecor::Button::Button(PWinObj *parent, Theme::PDecorButtonData *data,
 	_gm.width = width;
 	_gm.height = height;
 
-	XSetWindowAttributes attr;
-	attr.event_mask = EnterWindowMask|LeaveWindowMask;
-	attr.override_redirect = True;
-	_window =
-		X11::createWindow(_parent->getWindow(),
-				  -_gm.width, -_gm.height, _gm.width, _gm.height, 0,
-				  CopyFromParent, InputOutput, CopyFromParent,
-				  CWEventMask|CWOverrideRedirect, &attr);
+	_window = X11::createWmWindow(_parent->getWindow(),
+				      -_gm.width, -_gm.height,
+				      _gm.width, _gm.height,
+				      EnterWindowMask|LeaveWindowMask);
 	setState(_state);
 }
 
@@ -1735,9 +1731,8 @@ PDecor::applyBorderShape(int kind)
 void
 PDecor::applyBorderShapeNormal(int kind, bool client_shape)
 {
-	Window shape = X11::createSimpleWindow(X11::getRoot(),
-					       0, 0, _gm.width, _gm.height,
-					       0, 0, 0);
+	Window shape = X11::createWmWindow(X11::getRoot(),
+					   0, 0, _gm.width, _gm.height, None);
 	if (_child) {
 		X11::shapeCombine(shape, kind,
 				  bdLeft(this), bdTop(this) + titleHeight(this),
@@ -1773,15 +1768,15 @@ PDecor::applyBorderShapeShaded(int kind)
 {
 	Window shape;
 	if (_data->getTitleWidthMin()) {
-		shape =
-			X11::createSimpleWindow(X11::getRoot(),
-						0, 0,
-						_title_wo.getWidth(), _title_wo.getHeight(),
-						0, 0, 0);
+		shape = X11::createWmWindow(X11::getRoot(),
+					    0, 0,
+					    _title_wo.getWidth(),
+					    _title_wo.getHeight(),
+					    None);
 	} else {
-		shape =
-			X11::createSimpleWindow(X11::getRoot(),
-						0, 0, _gm.width, _gm.height, 0, 0, 0);
+		shape = X11::createWmWindow(X11::getRoot(),
+					    0, 0, _gm.width, _gm.height,
+					    None);
 		if (_border) {
 			applyBorderShapeBorder(kind, shape);
 		}

src/PMenu.cc
@@ -77,17 +77,13 @@ PMenu::PMenu(const std::string &title,
 
 	// create menu content child
 	_menu_wo = new PWinObj(false);
-	XSetWindowAttributes attr;
-	attr.override_redirect = True;
-	attr.event_mask =
+
+	long event_mask =
 		ButtonPressMask|ButtonReleaseMask|ButtonMotionMask|
 		ExposureMask|FocusChangeMask|KeyPressMask|KeyReleaseMask|
 		PointerMotionMask;
-	_menu_wo->setWindow(X11::createWindow(_window, 0, 0, 1, 1, 0,
-					      CopyFromParent, InputOutput,
-					      CopyFromParent,
-					      CWOverrideRedirect|CWEventMask,
-					      &attr));
+	Window window = X11::createWmWindow(_window, 0, 0, 1, 1, event_mask);
+	_menu_wo->setWindow(window);
 
 	titleAdd(&_title);
 	titleSetActive(0);

src/StatusWindow.cc
@@ -1,5 +1,6 @@
 //
 // StatusWindow.cc for pekwm
+// Copyright (C) 2017-2022 Claes Nästén <pekdon@gmail.com>
 // Copyright (C) 2004-2016 the pekwm development team
 //
 // This program is licensed under the GNU GPL.
@@ -30,14 +31,7 @@ StatusWindow::StatusWindow(Theme* theme)
 	setLayer(LAYER_NONE); // hack, goes over LAYER_MENU
 	_hidden = true; // don't care about it when changing worskpace etc
 
-	XSetWindowAttributes attr;
-	attr.event_mask = None;
-
-	_status_wo->setWindow(X11::createWindow(_window, 0, 0, 1, 1, 0,
-						CopyFromParent, CopyFromParent,
-						CopyFromParent,
-						CWEventMask, &attr));
-
+	_status_wo->setWindow(X11::createWmWindow(_window, 0, 0, 1, 1, None));
 	addChild(_status_wo);
 	activateChild(_status_wo);
 	_status_wo->mapWindow();

src/WorkspaceIndicator.cc
@@ -1,5 +1,6 @@
 //
 // WorkspaceIndicator.cc for pekwm
+// Copyright (C) 2021-2022 Claes Nästén <pekdon@gmail.com>
 // Copyright (C) 2009-2020 the pekwm development team
 //
 // This program is licensed under the GNU GPL.
@@ -27,13 +28,10 @@ WorkspaceIndicator::Display::Display(PWinObj *parent)
 	// Do not give the indicator focus, it doesn't handle input
 	_focusable = false;
 
-	XSetWindowAttributes attr;
-	attr.override_redirect = false;
-	attr.event_mask = ButtonPressMask|ButtonReleaseMask|ButtonMotionMask|
-		FocusChangeMask|KeyPressMask|KeyReleaseMask;
-	_window = X11::createWindow(_parent->getWindow(), 0, 0, 1, 1, 0,
-				    CopyFromParent, InputOutput, CopyFromParent,
-				    CWOverrideRedirect|CWEventMask, &attr);
+	long event_mask = ButtonPressMask|ButtonReleaseMask|ButtonMotionMask|
+			  FocusChangeMask|KeyPressMask|KeyReleaseMask;
+	_window = X11::createWmWindow(_parent->getWindow(), 0, 0, 1, 1,
+				      event_mask);
 }
 
 /**

src/X11.cc
@@ -1950,6 +1950,22 @@ X11::createSimpleWindow(Window parent,
 	return None;
 }
 
+/**
+ * Create window to be used by the window manager, having override redirect
+ * set to true.
+ */
+Window
+X11::createWmWindow(Window parent, int x, int y, uint width, uint height,
+		    int event_mask)
+{
+	XSetWindowAttributes attr;
+	attr.event_mask = event_mask;
+	attr.override_redirect = True;
+	return createWindow(parent, x, y, width, height, 0,
+			    CopyFromParent, InputOutput, CopyFromParent,
+			    CWEventMask|CWOverrideRedirect, &attr);
+}
+
 void
 X11::destroyWindow(Window win)
 {

commit 4edd88ae48aa5e3f9a794a13b3543fa3ac0b73f2

X11::createWmWindow extended with uint _class attribute. Commit ac6a6378867c84148bf5f5d77eabb3bfcdea7a81 caused visual artifacts at top-left side of screen due to EdgeWO::EdgeWO @ ManagerWindows.cc opened windows with InputOutput mode. Now, it can open window InputOnly mode again.

src/DockApp.cc
@@ -83,7 +83,7 @@ DockApp::DockApp(Window win) :
 
 	// create parent window which is going to hold the border
 	_window = X11::createWmWindow(X11::getRoot(),
-				      _gm.x, _gm.y, _gm.width, _gm.height,
+				      _gm.x, _gm.y, _gm.width, _gm.height, InputOutput,
 				      SubstructureRedirectMask|ButtonPressMask|
 				      ButtonMotionMask);
 

src/InputDialog.cc
@@ -133,7 +133,7 @@ InputDialog::InputDialog(const std::string &title)
 	_text_wo = new PWinObj(true);
 
 	Window window =
-		X11::createWmWindow(_window, 0, 0, 1, 1,
+		X11::createWmWindow(_window, 0, 0, 1, 1, InputOutput,
 				    ButtonPressMask|ButtonReleaseMask|
 				    ButtonMotionMask|FocusChangeMask|
 				    KeyPressMask|KeyReleaseMask);

src/ManagerWindows.cc
@@ -42,7 +42,7 @@ HintWO::HintWO(Window root)
 	_type = WO_SCREEN_HINT;
 	setLayer(LAYER_NONE);
 
-	_window = X11::createWmWindow(X11::getRoot(), -200, -200, 5, 5,
+	_window = X11::createWmWindow(X11::getRoot(), -200, -200, 5, 5, InputOutput,
 				      PropertyChangeMask);
 
 	// Set hints not being updated
@@ -585,7 +585,7 @@ EdgeWO::EdgeWO(RootWO* root_wo, EdgeType edge, bool set_strut,
 	_iconified = true; // hack, to be ignored when placing
 	_focusable = false; // focusing input only windows crashes X
 
-	_window = X11::createWmWindow(root_wo->getWindow(), 0, 0, 1, 1,
+	_window = X11::createWmWindow(root_wo->getWindow(), 0, 0, 1, 1, InputOnly,
 				      EnterWindowMask|LeaveWindowMask|
 				      ButtonPressMask|ButtonReleaseMask);
 

src/PDecor.cc
@@ -51,7 +51,7 @@ PDecor::Button::Button(PWinObj *parent, Theme::PDecorButtonData *data,
 
 	_window = X11::createWmWindow(_parent->getWindow(),
 				      -_gm.width, -_gm.height,
-				      _gm.width, _gm.height,
+				      _gm.width, _gm.height, InputOutput,
 				      EnterWindowMask|LeaveWindowMask);
 	setState(_state);
 }
@@ -1732,7 +1732,7 @@ void
 PDecor::applyBorderShapeNormal(int kind, bool client_shape)
 {
 	Window shape = X11::createWmWindow(X11::getRoot(),
-					   0, 0, _gm.width, _gm.height, None);
+					   0, 0, _gm.width, _gm.height, InputOutput, None);
 	if (_child) {
 		X11::shapeCombine(shape, kind,
 				  bdLeft(this), bdTop(this) + titleHeight(this),
@@ -1771,11 +1771,11 @@ PDecor::applyBorderShapeShaded(int kind)
 		shape = X11::createWmWindow(X11::getRoot(),
 					    0, 0,
 					    _title_wo.getWidth(),
-					    _title_wo.getHeight(),
+					    _title_wo.getHeight(), InputOutput,
 					    None);
 	} else {
 		shape = X11::createWmWindow(X11::getRoot(),
-					    0, 0, _gm.width, _gm.height,
+					    0, 0, _gm.width, _gm.height, InputOutput,
 					    None);
 		if (_border) {
 			applyBorderShapeBorder(kind, shape);

src/PMenu.cc
@@ -82,7 +82,7 @@ PMenu::PMenu(const std::string &title,
 		ButtonPressMask|ButtonReleaseMask|ButtonMotionMask|
 		ExposureMask|FocusChangeMask|KeyPressMask|KeyReleaseMask|
 		PointerMotionMask;
-	Window window = X11::createWmWindow(_window, 0, 0, 1, 1, event_mask);
+	Window window = X11::createWmWindow(_window, 0, 0, 1, 1, InputOutput, event_mask);
 	_menu_wo->setWindow(window);
 
 	titleAdd(&_title);

src/StatusWindow.cc
@@ -31,7 +31,7 @@ StatusWindow::StatusWindow(Theme* theme)
 	setLayer(LAYER_NONE); // hack, goes over LAYER_MENU
 	_hidden = true; // don't care about it when changing worskpace etc
 
-	_status_wo->setWindow(X11::createWmWindow(_window, 0, 0, 1, 1, None));
+	_status_wo->setWindow(X11::createWmWindow(_window, 0, 0, 1, 1, CopyFromParent, None));
 	addChild(_status_wo);
 	activateChild(_status_wo);
 	_status_wo->mapWindow();

src/WorkspaceIndicator.cc
@@ -30,7 +30,7 @@ WorkspaceIndicator::Display::Display(PWinObj *parent)
 
 	long event_mask = ButtonPressMask|ButtonReleaseMask|ButtonMotionMask|
 			  FocusChangeMask|KeyPressMask|KeyReleaseMask;
-	_window = X11::createWmWindow(_parent->getWindow(), 0, 0, 1, 1,
+	_window = X11::createWmWindow(_parent->getWindow(), 0, 0, 1, 1, InputOutput,
 				      event_mask);
 }
 

src/X11.cc
@@ -1955,14 +1955,14 @@ X11::createSimpleWindow(Window parent,
  * set to true.
  */
 Window
-X11::createWmWindow(Window parent, int x, int y, uint width, uint height,
+X11::createWmWindow(Window parent, int x, int y, uint width, uint height, uint _class,
 		    ulong event_mask)
 {
 	XSetWindowAttributes attr;
 	attr.event_mask = event_mask;
 	attr.override_redirect = True;
 	return createWindow(parent, x, y, width, height, 0,
-			    CopyFromParent, InputOutput, CopyFromParent,
+			    CopyFromParent, _class, CopyFromParent,
 			    CWEventMask|CWOverrideRedirect, &attr);
 }
 

src/X11.hh
@@ -446,7 +446,7 @@ public:
 					 uint border_width,
 					 ulong border, ulong background);
 	static Window createWmWindow(Window parent,
-				     int x, int y, uint width, uint height,
+				     int x, int y, uint width, uint height, uint _class,
 				     ulong event_mask);
 	static void destroyWindow(Window win);
 	static void changeWindowAttributes(Window win, ulong mask,