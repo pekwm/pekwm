d4771c538507d4fddd0fb01a1a002523d540e711
Claes Nästén <pekdon@gmail.com>
2022-12-21 09:05:23 +0100

    Fix PEKWM_HAVE_LOCALE_COMBINE check type (extra 1)
    Fix the check for it in CMake and only run the test for it if it was
    found during configuration.

--- test/test_Charset.hh
+++ test/test_Charset.hh
@@ -1,6 +1,6 @@
 //
 // test_Charset.cc for pekwm
-// Copyright (C) 2021 Claes Nästén <pekdon@gmail.com>
+// Copyright (C) 2021-2022 Claes Nästén <pekdon@gmail.com>
 //
 // This program is licensed under the GNU GPL.
 // See the LICENSE file for more information.
@@ -9,6 +9,7 @@
 #include <iostream>
 
 #include "test.hh"
+#include "config.h"
 #include "Charset.hh"
 
 class TestCharset : public TestSuite {
@@ -21,7 +22,9 @@ public:
 private:
 	static void testToSystem(void);
 	static void testFromSystem(void);
+#ifdef PEKWM_HAVE_LOCALE_COMBINE
 	static void test_no_grouping_numpunct(void);
+#endif // PEKWM_HAVE_LOCALE_COMBINE
 };
 
 TestCharset::TestCharset(void)
@@ -36,10 +39,13 @@ TestCharset::~TestCharset(void)
 bool
 TestCharset::run_test(TestSpec spec, bool status)
 {
+#ifdef PEKWM_HAVE_LOCALE_COMBINE
 	TEST_FN(spec, "no_grouping_numpunct", test_no_grouping_numpunct());
+#endif // PEKWM_HAVE_LOCALE_COMBINE
 	return status;
 }
 
+#ifdef PEKWM_HAVE_LOCALE_COMBINE
 void
 TestCharset::test_no_grouping_numpunct(void)
 {
@@ -47,3 +53,4 @@ TestCharset::test_no_grouping_numpunct(void)
 	oss << 100200300;
 	ASSERT_EQUAL("no grouping", "100200300", oss.str());
 }
+#endif // PEKWM_HAVE_LOCALE_COMBINE
